d30130741f6a8438ffc071a809bba97706feafbf
Robert Stepanov <robertdstepanov@gmail.com>
2019-12-03 20:42:55 -0500

    MVP no Stretch

--- js/index.js
+++ js/index.js
@@ -107,6 +107,72 @@ logo_heading.addEventListener("copy", event => {
   event.preventDefault();
 });
 
+//  --------------- Drag/Drop Event  ------------------------->
+
+let dragged;
+
+document.addEventListener("drag", function(event) {}, false);
+
+document.addEventListener(
+  "dragstart",
+  function(event) {
+    dragged = event.target;
+
+    event.target.style.opacity = 0.5;
+  },
+  false
+);
+
+document.addEventListener(
+  "dragend",
+  function(event) {
+    event.target.style.opacity = "";
+  },
+  false
+);
+
+document.addEventListener(
+  "dragover",
+  function(event) {
+    event.preventDefault();
+  },
+  false
+);
+
+document.addEventListener(
+  "dragenter",
+  function(event) {
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "orange";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "dragleave",
+  function(event) {
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "drop",
+  function(event) {
+    event.preventDefault();
+
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+    }
+  },
+  false
+);
+
 //  -----------------   Nav Prevent Default  ------------------>
 
 const stopNav = document.querySelectorAll(".nav-link");
